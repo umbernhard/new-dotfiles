4ec6a175f5b62b39cf59c6fb80bc1ca000b96aec
Matt Bernhard <mdb92nc@gmail.com>
2023-02-03 21:54:31 -0500

    removing unneeded files

--- kernel.nix
+++
@@ -1,9 +0,0 @@
-" ============================================================================
-" Netrw Directory Listing                                        (netrw v171)
-"   /home/matt/Workspace/dotfiles/system/boot
-"   Sorted by      name
-"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
-"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
-" ==============================================================================
-../                              0 Fri 03 Feb 2023 08:09:39 PM EST
-./                               0 Fri 03 Feb 2023 08:09:01 PM EST

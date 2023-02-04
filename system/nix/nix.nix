1ad925db22a20a51b190fb5417f8d654fa284602
Matt Bernhard <mdb92nc@gmail.com>
2023-02-03 22:10:29 -0500

    fixing systemd-boot path

--- system/nix/nix.nix
+++ system/nix/nix.nix
@@ -7,7 +7,7 @@
     settings.trusted-substituters = ["http://cache.nixos.org"]; 
     settings.substituters = ["http://cache.nixos.org"];
     gc.automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+    gc.dates = "weekly";
+    gc.options = "--delete-older-than 30d";
   };
 }

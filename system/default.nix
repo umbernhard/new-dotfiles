d42a62b8e30e47aea9e8229b84c4d3b2a60f9d04
Matt Bernhard <mdb92nc@gmail.com>
2023-02-05 14:13:09 -0500

    helps to add the module...

--- system/default.nix
+++ system/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [  ./hardware-configuration.nix ./network.nix ./boot ./nix ./services ./user ./desktops ./services ];
+  imports = [  ./hardware-configuration.nix ./network.nix ./boot ./nix ./fonts ./services ./user ./desktops ./services ];
   i18n.defaultLocale = "en_US.UTF-8";
   system.stateVersion = "22.11";
   time.timeZone = "America/Detroit";

985f3bc78dc674e14d2a5fdd9a71512f7474d163
Matt Bernhard <mdb92nc@gmail.com>
2023-03-23 15:23:05 -0400

    forgot to add tlp service

--- system/services/default.nix
+++ system/services/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ./libvirt.nix ./xserver ./fprint.nix ./bluetooth.nix ];
+  imports = [ ./libvirt.nix ./xserver ./fprint.nix ./bluetooth.nix ./tlp.nix ];
 }

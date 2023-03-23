c010821e0a32850fee3fa7869abebe62c1389bb9
Matt Bernhard <mdb92nc@gmail.com>
2023-03-23 14:08:18 -0400

    More tweaks

--- system/default.nix
+++ system/default.nix
@@ -3,6 +3,6 @@
 {
   imports = [  ./hardware-configuration.nix ./network.nix ./boot ./nix ./fonts ./services ./user ./desktops ./services ];
   i18n.defaultLocale = "en_US.UTF-8";
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.05";
   time.timeZone = "America/Detroit";
 }
--- system/desktops/default.nix
+++ system/desktops/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [./gnome.nix];
+  imports = [ ./gnome.nix ];
 }
